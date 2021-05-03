c29d74586ad4c0add90afd148a97097f137661a8
jrmcgarvey <jmcgarve@bellsouth.net>
2021-05-03 19:17:59 -0400

    associations lesson completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :pages
   get '/pages', to: 'pages#index'
   post '/pages', to: 'pages#create'
   get '/pages/new', to: 'pages#new', as: 'new_page'
@@ -8,5 +11,11 @@ Rails.application.routes.draw do
   get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
   patch '/pages/:id', to: 'pages#update'
   delete '/pages/:id', to: 'pages#destroy'
-  #resources :pages
+  #get '/posts/:post_id/comments', to: 'comments#index'
+  #post '/posts/:post_id/comments', to: 'comments#create'
+  #get '/posts/:post_id/comments/new', to: 'comments#new', as: 'new_comment'
+  #get '/posts/:post_id/comments/:id', to: 'comments#show', as: 'comment'
+  #get '/posts/:post_id/comments/:id/edit', to: 'comments#edit', as: 'edit_comment'
+  #patch '/posts/:post_id/comments/:id', to: 'comments#update'
+  #delete '/posts/:post_id/comments/:id', to: 'comments#destroy'
 end
